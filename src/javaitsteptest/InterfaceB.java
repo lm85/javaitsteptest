0ccaac6c5b75da8c7dfde29bcf1c0d17a0c77d13
Director itSTEP <Director itSTEP@192.168.0.74>
2018-02-06 16:09:14 +0100

    InterfaceB

---
+++ src/javaitsteptest/InterfaceB.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaitsteptest;
+
+/**
+ *
+ * @author Director itSTEP
+ */
+public interface InterfaceB {
+    public void chlup(); 
+}
